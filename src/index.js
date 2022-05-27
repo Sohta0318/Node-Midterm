f6d6d0f64223994195310133ad8e30eddffdebe7
SOHTA0318 <nicosohta0318@gmail.com>
2022-05-27 14:27:48 +0900

    final

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ require("./db/mongoose");
 const userRouter = require("./routes/user");
 const blogRouter = require("./routes/blog");
 const favoriteRouter = require("./routes/favorite");
-// const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 const path = require("path");
 
 const app = express();
@@ -13,7 +13,7 @@ app.use(express.json());
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
 app.use(express.static(__dirname + "/public"));
-// app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(userRouter);
 app.use(blogRouter);
 app.use(favoriteRouter);
